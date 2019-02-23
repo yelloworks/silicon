166f4eef7dc982311e1ea7182d52445e9922b77b
a.rogov <a.rogov@intelprime.com>
2019-02-23 16:58:12 +0300

    Categories counting added

---
+++ siliconTestMVC5/siliconTestMVC5/Models/ViewCategoriesModel.cs
@@ -0,0 +1,14 @@
+﻿using siliconTestMVC5.Models.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace siliconTestMVC5.Models
+{
+    public class ViewCategoriesModel
+    {
+        public int ChildredCount { get; set; }
+        public Category Item { get; set; }
+    }
+}
